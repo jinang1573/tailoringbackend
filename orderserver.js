aa99ab8d9003e6dd963cacb35d5f34217e419f06
Jinang shah <jinangs47@gmail.com>
2025-03-06 15:26:55 +0530

    Update MongoDB connection string with new password and update order routes

--- orderserver.js
+++ orderserver.js
@@ -11,7 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/tailor', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://jinangs47:pass1234@cluster0.o7tzg.mongodb.net/tailor?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Failed to connect to MongoDB', err));
 
